7fe8ef6042b2c4116c2862df21950c4309c57291
LeonidMarushkin <139209971+LeonidMarushkin@users.noreply.github.com>
2024-01-29 21:18:20 +0300

    project clear

---
+++ src/views/MainPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+// import { ref } from 'vue'
+
+export default {
+  name: 'MainPage',
+  components: {
+  },
+  props: {
+  },
+  setup () {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
