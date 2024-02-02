54c2fb9cc4759ffbb06cbad8b792c0c82c55359f
LeonidMarushkin <139209971+LeonidMarushkin@users.noreply.github.com>
2024-02-03 00:35:13 +0300

    first store

--- src/components/elements/CardProductComponent.vue
+++ src/components/elements/CardProductComponent.vue
@@ -10,7 +10,7 @@
         <div class="card__price">
           <span>{{ price }}</span>
         </div>
-        <ButtonComponent fontawesomeIcon='fa-solid fa-plus fa-2xs' isMain iconShow/>
+        <ButtonComponent fontawesomeIcon='fa-solid fa-plus fa-2xs' isMain iconShow />
       </div>
     </div>
     <div v-else class="cardBasket">
@@ -22,7 +22,7 @@
         <div class="card__price">
           <span>{{ price }}</span>
         </div>
-        <ButtonComponent fontawesomeIcon='fa-solid fa-plus fa-2xs' isBasketCard iconShow isRotated/>
+        <ButtonComponent fontawesomeIcon='fa-solid fa-plus fa-2xs' isBasketCard iconShow isRotated />
       </div>
     </div>
   </div>
